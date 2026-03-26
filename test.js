e8a8fe18907a83eaae1fa41f9d020b1758b11bb9
Rolf Loges <rolf@loges.org>
2026-03-26 08:03:28 +0100

    chore: replace mocha, chai and nyc with jest

--- test.js
+++
@@ -1,4 +0,0 @@
-const Mocha = require('mocha')
-const mocha = new Mocha({})
-mocha.addFile('urldecode-latin1.spec')
-mocha.run()
--- urldecode-latin1.spec.js
+++ urldecode-latin1.spec.js
@@ -1,24 +1,21 @@
-const describe = require('mocha').describe
-const it = require('mocha').it
-const expect = require('chai').expect
 const urldecodeLatin1 = require('./urldecode-latin1')
 
 describe('urldecodeLatin1', () => {
   it('should be defined', () => {
-    expect(urldecodeLatin1).to.be.a('Function')
+    expect(urldecodeLatin1).toBeInstanceOf(Function)
   })
   it('decode from Buffer should resolve', () => {
     const b = Buffer.from([0x6B, 0x3D, 0xDF])
-    expect(urldecodeLatin1(b).k).equals('ß')
+    expect(urldecodeLatin1(b).k).toBe('ß')
   })
   it('decode from string should resolve', () => {
     const s = 'k=%61%0A%AE%DF%FF'
-    expect(urldecodeLatin1(s).k).equals('a\n®ßÿ')
+    expect(urldecodeLatin1(s).k).toBe('a\n®ßÿ')
   })
   it('decode from incomplete string should throw error', () => {
     let s = 'k=%61%AE%D'
-    expect(() => urldecodeLatin1(s)).to.throw(/incomplete/)
+    expect(() => urldecodeLatin1(s)).toThrow(/incomplete/)
     s = 'k=%61%AE%'
-    expect(() => urldecodeLatin1(s)).to.throw(/incomplete/)
+    expect(() => urldecodeLatin1(s)).toThrow(/incomplete/)
   })
 })
